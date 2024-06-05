06ac2d374cf11ee56a80108bf5bf3a71d1839d32
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-06-05 11:05:38 +0300

    update source/Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -77,7 +77,7 @@ class Main extends Sprite
 
 		// Credits to MAJigsaw77 (he's the og author for this code)
 		#if android
-		Sys.setCwd(Path.addTrailingSlash(/*Context.getExternalFilesDir()*/SUtil.getPath()));
+		Sys.setCwd(Path.addTrailingSlash(SUtil.getPath()));
 		#elseif ios
 		Sys.setCwd(lime.system.System.applicationStorageDirectory);
 		#end
