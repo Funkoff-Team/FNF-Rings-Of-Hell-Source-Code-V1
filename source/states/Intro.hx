9e29823fc31116da1c5ad0faa1bd429a4fd246d5
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-06-08 21:30:26 +0300

    update source/states/Intro.hx

--- source/states/Intro.hx
+++ source/states/Intro.hx
@@ -17,31 +17,22 @@ using StringTools;
 
 class Intro extends MusicBeatState
 {
-	public var finishCallback:Void->Void;
 	  var cutVid:Video = new Video();
 		var foundFile:Bool = false;
 
     override public function create()
     {
 		var fileName:String = Paths.video('newgroundsintro');
-		
-    	#if sys
-    	if (FileSystem.exists(fileName))
-    	#else
+
     	if (OpenFlAssets.exists(fileName))
-    	#end 
     	foundFile = true;
 
 		if(foundFile) {
 		  cutVid.startVideo(fileName);
 			cutVid.onVideoEnd.addOnce(() -> {
-	   	if (finishCallback != null)
-			finishCallback();
 			 MusicBeatState.switchState(new TitleState()); 
 			});
     } else {
-		if (finishCallback != null)
-			finishCallback();
 			FlxG.log.warn('Couldnt find video file: ' + fileName);
 		}
   }
