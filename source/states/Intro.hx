5a3fa618203eb1a04fccd3f54d16c994c43ea5e2
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-06-08 11:20:17 +0300

    update source/states/Intro.hx

--- source/states/Intro.hx
+++ source/states/Intro.hx
@@ -1,10 +1,14 @@
 package states;
 
 #if VIDEOS_ALLOWED
+#if hxCodec
 #if (hxCodec >= "3.0.0") import hxcodec.flixel.FlxVideo as VideoHandler;
 #elseif (hxCodec >= "2.6.1") import hxcodec.VideoHandler as VideoHandler;
 #elseif (hxCodec == "2.6.0") import VideoHandler;
 #else import vlc.MP4Handler as VideoHandler; #end
+#elseif hxvlc
+import hxvlc.flixel.FlxVideo as VideoHandler;
+#end
 #end
 
 import states.TitleState;
@@ -15,8 +19,9 @@ class Intro extends MusicBeatState
 {
     override public function create()
     {
+		var filePath:String = Paths.video('newgroundsintro');
+		#if hxCodec
     var video:VideoHandler = new VideoHandler();
-		var filepath:String = Paths.video('newgroundsintro');
 
       #if (hxCodec >= "3.0.0")
 			video.play(filepath);
@@ -31,6 +36,48 @@ class Intro extends MusicBeatState
    		{
        MusicBeatState.switchState(new TitleState());
   }
-  #end
+  #elseif hxvlc
+  playIntro(filePath);
+  function playIntro(filePath:String) {
+    var intro:FlxVideo;
+    // Video displays OVER the FlxState.
+    intro = new FlxVideo();
+
+    if (intro != null)
+    {
+      intro.bitmap.onEndReached.add(onIntroEnd);
+      add(intro);
+
+      openfl.Assets.loadBytes(filePath).onComplete(function(bytes:openfl.utils.ByteArray):Void
+      {
+        if (intro.load(bytes))
+          intro.play();
+
+        onVideoStarted.dispatch();
+      });
     }
+    else
+    {
+      trace('ALERT: Video is null! Could not play cutscene!');
+    }
+  }
+  #end
+  }
+
+	override function update(elapsed:Float)
+	{
+  #if hxvlc
+  function onIntroEnd():Void
+  {
+    if (intro != null)
+    {
+      intro.stop();
+      remove(intro);
+    }
+    intro.destroy();
+    intro = null;
+    MusicBeatState.switchState(new TitleState()); 
+  }
+  #end
+	}
 }
