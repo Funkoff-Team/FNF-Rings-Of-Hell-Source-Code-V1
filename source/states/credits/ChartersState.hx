8e2f85008af01d0ea2191027c5699a911c2a33d3
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-06-15 08:54:55 +0300

    update source/states/credits/ChartersState.hx

--- source/states/credits/ChartersState.hx
+++ source/states/credits/ChartersState.hx
@@ -112,6 +112,9 @@ class ChartersState extends MusicBeatState
 		bg.color = getCurrentBGColor();
 		intendedColor = bg.color;
 		changeSelection();
+    #if mobile
+    addVirtualPad(UP_DOWN, A_B);
+    #end
 		super.create();
 	}
 
