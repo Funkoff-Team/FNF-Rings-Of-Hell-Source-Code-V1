c33d9eb91a67a9556131f633e96d310168d7371f
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-06-18 12:52:33 +0300

    update source/states/FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -109,9 +109,9 @@ class FreeplayState extends MusicBeatState
 		add(boxedShit);
 
 		var folderNum:Int = 0;
-		for (i in folderList)
+		for (i in 0...folderList)
 		{
-			if (Assets.exists(Paths.getPath('data/${i}/${i}.json', TEXT)) || Assets.exists(Paths.getPath('data/${i}/${i}-hard.json', TEXT)) || Assets.exists(Paths.getPath('data/${i}/${i}-null.json', TEXT)))
+			if (Assets.exists(Paths.getPath('data/${folderList[i]}/${folderList[i]}.json', TEXT)) || Assets.exists(Paths.getPath('data/${folderList[i]}/${folderList[i]}-hard.json', TEXT)) || Assets.exists(Paths.getPath('data/${folderList[i]}/${folderList[i]}-null.json', TEXT)))
 			{
 				var boxLol:FlxSkewedSprite = new FlxSkewedSprite((folderNum * 420), 0);
 				boxLol.loadGraphic(Paths.image('menus/EYX/freeplay/FreeBox'));
